4be56bfaff9116c44d6470f0cb8914bf1a7df81d
KarakozovaRegina <105420067+KarakozovaRegina@users.noreply.github.com>
2023-06-23 19:42:17 +0300

    Add files via upload

---
+++ PCP3/ClientT.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include "Winsock2.h" // заголовок WS2_32.dll
+#pragma comment(lib, "WS2_32.lib") // экспорт WS2_32.dll
+#pragma warning(disable:4996) //для использования inet_addr()
+
+using namespace std;
+
+string GetErrorMsgText(int code) // cформировать текст ошибки
+{
+    string msgText;
+    switch (code) // проверка кода возврата 
+    {
+    case WSAEINTR: msgText = "WSAEINTR"; break;
+    case WSAEACCES: msgText = "WSAEACCES"; break;
+        //..........коды WSAGetLastError ..........................
+    case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE";break;
+    default: msgText = "***ERROR***"; break;
+    };
+    return msgText;
+};
+string SetErrorMsgText(string msgText, int code)
+{
+    return msgText + GetErrorMsgText(code);
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    WSADATA wsaData;
+    SOCKET clientSocket; // клиентский сокет
+        //............................................................
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup:", WSAGetLastError());
+        //............................................................
+        if ((clientSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("socket:", WSAGetLastError());
+        
+        //............................................................
+        SOCKET serverSocket;
+
+        SOCKADDR_IN serv; // параметры сокета сервера
+        serv.sin_family = AF_INET; // используется ip-адресация 
+        serv.sin_port = htons(2000); // порт 2000
+        serv.sin_addr.s_addr = inet_addr("127.0.0.1"); // адрес сервера 
+        char obuf[50] = "Hello from ClientU"; //буфер вывода
+        char ibuf[50];                    //буфер ввода 
+        int lobuf = 0; //количество отправленных 
+        int  libuf = 0;                    //количество принятых байт
+        //...................передача-клиент...........................
+      
+
+        while (true) {
+           
+            cout << "Введите кол-во сообщений: ";
+            int count;
+            cin >> count;
+
+            for (int i = 0; i < count; i++) {
+
+                if ((lobuf = sendto(clientSocket, obuf, strlen(obuf) + 1, NULL, (sockaddr*)&serv, sizeof(serv))) == SOCKET_ERROR) // отправить сообщение
+                    throw  SetErrorMsgText("send:", WSAGetLastError());
+                if ((libuf = recv(clientSocket, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR)
+                    throw  SetErrorMsgText("recv:", WSAGetLastError());
+
+                cout << " Сообщение от сервера " << ": " << ibuf <<" " << (i + 1) << endl;
+            }
+            //............................................................
+            if (closesocket(clientSocket) == SOCKET_ERROR)
+                throw SetErrorMsgText("closesocket:", WSAGetLastError());
+            //.............................................................
+            if (WSACleanup() == SOCKET_ERROR)
+                throw SetErrorMsgText("Cleanup:", WSAGetLastError());
+        }
+      /* .......................................................
+        for (int i = 0; i < count; i++) {
+            if ((lobuf = send(cC, obuf, strlen(obuf) + 1, NULL)) == SOCKET_ERROR)
+                throw  SetErrorMsgText("send:", WSAGetLastError());
+
+            if ((libuf = recv(cC, ibuf, sizeof(ibuf), NULL)) == SOCKET_ERROR)
+                throw  SetErrorMsgText("recv:", WSAGetLastError());
+
+            cout << " Сообщение от сервера " << ": " << ibuf << (i + 1) << endl;
+
+
+        }
+
+        if (SOCKET_ERROR == send(cC, "Конец передачи", sizeof("Конец передачи"), NULL))
+        {
+            cout << "send exit : " << GetLastError() << endl;
+        }*/
+       
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << "WSAGetLastError: " << errorMsgText;
+    }
+    //................................................................
+    return 0;
+}
---
+++ PCP3/ServerT.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include "Winsock2.h" // заголовок WS2_32.dll
+#pragma comment(lib, "WS2_32.lib") // экспорт WS2_32.dll
+#pragma warning( disable : 4996)//for inet_ntoa
+using namespace std;
+
+
+string GetErrorMsgText(int code) // cформировать текст ошибки
+{
+    string msgText;
+    switch (code) // проверка кода возврата 
+    {
+    case WSAEINTR: msgText = "WSAEINTR"; break;
+    case WSAEACCES: msgText = "WSAEACCES"; break;
+        //..........коды WSAGetLastError ..........................
+    case WSASYSCALLFAILURE: msgText = "WSASYSCALLFAILURE";break;
+    default: msgText = "***ERROR***"; break;
+    };
+    return msgText;
+};
+string SetErrorMsgText(string msgText, int code)
+{
+    return msgText + GetErrorMsgText(code);
+};
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    WSADATA wsaData;
+    SOCKET serverSocket; // клиентский сокет
+    SOCKADDR_IN clnt;//структура сокета клиента
+
+    int i;
+    //............................................................
+    try
+    {
+        if (WSAStartup(MAKEWORD(2, 0), &wsaData) != 0)
+            throw SetErrorMsgText("Startup:", WSAGetLastError());
+        //............................................................
+        if ((serverSocket = socket(AF_INET, SOCK_DGRAM, NULL)) == INVALID_SOCKET)
+            throw SetErrorMsgText("socket:", WSAGetLastError());
+        //............................................................
+  
+        SOCKADDR_IN serv; // параметры сокета sS
+        serv.sin_family = AF_INET; // используется IP-адресация 
+        serv.sin_port = htons(2000); // порт 2000 
+        serv.sin_addr.s_addr = INADDR_ANY; // адрес сервера
+       if (bind(serverSocket, (LPSOCKADDR)&serv, sizeof(serv)) == SOCKET_ERROR)
+            throw SetErrorMsgText("bind:", WSAGetLastError());
+
+        SOCKADDR_IN clnt; // параметры сокета клиента
+        memset(&clnt, 0, sizeof(clnt)); // обнулить память
+        int lc = sizeof(clnt);
+        char ibuf[50]; //буфер ввода 
+      
+
+        int libuf = 0; // кол-во принятых байт
+        int lobuf = 0;
+        while (true) {
+            int i = 0;
+
+           while (true) 
+           {
+
+               Sleep(3000);
+                if ((libuf = recvfrom(serverSocket, ibuf, sizeof(ibuf), NULL, (sockaddr*)&clnt, &lc)) == SOCKET_ERROR)
+                    throw  SetErrorMsgText("recv:", WSAGetLastError());
+
+      
+
+                cout << i <<  "	Сообщение: " << ibuf << endl;
+                i++;
+             
+
+                if ((lobuf = sendto(serverSocket, ibuf, strlen(ibuf) + 1, NULL, (sockaddr*)&clnt, sizeof(clnt))) == SOCKET_ERROR) // отправить сообщение
+                    throw  SetErrorMsgText("send:", WSAGetLastError());
+            
+            }
+            //............................................................
+            if (closesocket(serverSocket) == SOCKET_ERROR)
+                throw SetErrorMsgText("closesocket:", WSAGetLastError());
+            //.............................................................
+            if (WSACleanup() == SOCKET_ERROR)
+                throw SetErrorMsgText("Cleanup:", WSAGetLastError());
+        }
+      
+    }
+    catch (string errorMsgText)
+    {
+        cout << endl << "WSAGetLastError: " << errorMsgText;
+    }
+    //................................................................
+    return 0;
+}
+
